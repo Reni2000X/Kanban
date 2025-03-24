0a45bad55e7fbd0b7bfab04b7a2c82aaf5d0e790
Ruben Ray Reni <reni@globaloc.local>
2025-03-24 17:53:33 +0100

    Status 24.03.

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,18 +4,32 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 # Create your views here.
 from django.shortcuts import render
+#from dashboard.models import Board 
+import json
+from django.http import JsonResponse
 
 def index(request):
     print("User joined the dashboard")
     return render(request, 'dashboard/index.html')
 
+from django.shortcuts import render
+
 def index_new(request):
     print("User joined the dashboard")
-    return render(request, 'dashboard/new_index.html')
+
+    x=Board.objects.all()
+    Board_names=[]
+    for board_instance in x: 
+        
+        a=board_instance.name
+        Board_names.append(a)
+    return render(request, 'dashboard/new_index.html', {'x': x})
+
 
 def create_new_project(request):
-    
-    new_project = Board(name="test project1")
+    data = json.loads(request.body)
+    print(data)
+    new_project = Board(name=data['name'])
     new_project.save()
     print("New project created")
 
